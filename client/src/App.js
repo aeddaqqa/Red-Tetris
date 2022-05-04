96a3b80ae0970da4191c50e9b3490f47df143014
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-05-04 13:23:52 +0000

    init unitary test

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,20 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import io from "socket.io-client";
-import styled, { ThemeProvider } from "styled-components";
-import "react-toastify/dist/ReactToastify.css";
-import Rooms from "./views/Rooms";
-import { Theme } from "./utils/theme";
-import NavBar from "./components/NavBar/NavBar";
-import NoMatch from "./components/NoMatch";
-import Game from "./views/Game";
-import Home from "./views/Home";
-import { startConnecting, isConnected } from "./store/slices/connectionSlice";
-import { Outlet, Route, Routes, Navigate } from "react-router";
-
-// const socket = io.connect("http://localhost:3001");
-
-const StyledApp = styled.div`
-    width: 100vw;
-    height: auto;
-    height: 100vh;
-    background-color: ${(props) => props.theme.background.primary};
-`;
-
-const Layout = () => {
-    return (
-        <StyledApp>
-            <Outlet />
-        </StyledApp>
-    );
-};
-
-const ProtectedRoute = ({ children }) => {
-    const player = useSelector((state) => state.player);
-    if (player.userName) return <Outlet />;
-    return <Navigate to="/home" />;
-};
+import "./App.css";
 
 function App() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(startConnecting());
-    }, [dispatch]);
-
-    return (
-        <ThemeProvider theme={Theme}>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route index path="home" element={<Home />} />
-                    <Route element={<ProtectedRoute />}>
-                        <Route path="rooms" element={<Rooms />} />
-                        <Route path="game" element={<Game />} />
-                    </Route>
-                    <Route path="*" element={<NoMatch />} />
-                </Route>
-            </Routes>
-        </ThemeProvider>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { store } from "./store/index.js";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
