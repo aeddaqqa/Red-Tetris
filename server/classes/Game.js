53bd69bed6a312e642656eba837204a90d7e48f8
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-05-27 18:36:38 +0100

    finish server

--- server/classes/Game.js
+++ server/classes/Game.js
@@ -52,72 +52,102 @@ class GamesRoom {
         });
     };
 
-    	/*
-	 ** Get room users name list
-	 */
-	getroomUsersDetails = (io, room, players) => {
-		return new Promise((resolve, reject) => {
-			const player = [];
-			const roomList = [];
-			const clientsList = io.sockets.adapter.rooms.get(room);
-			if (clientsList) {
-				for (const clientId of clientsList) {
-					player.push(clientId);
-				}
-				for (let i = 0; i < players.length; i++) {
-					for (let j = 0; j < player.length; j++) {
-						if (players[i].socketId === player[j]) {
-							roomList.push(players[i]);
-						}
-					}
-				}
-			}
-			resolve(roomList);
-		});
-	};
+    /*
+     ** Get room users name list
+     */
+    getroomUsersDetails = (io, room, players) => {
+        return new Promise((resolve, reject) => {
+            const player = [];
+            const roomList = [];
+            const clientsList = io.sockets.adapter.rooms.get(room);
+            console.log("lista", clientsList);
+            if (clientsList) {
+                for (const clientId of clientsList) {
+                    player.push(clientId);
+                }
+                for (let i = 0; i < players.length; i++) {
+                    for (let j = 0; j < player.length; j++) {
+                        if (players[i].socketId === player[j]) {
+                            roomList.push(players[i]);
+                        }
+                    }
+                }
+            }
+            console.log("roomlist", roomList);
+            resolve(roomList);
+        });
+    };
 
     /*
-	 ** Handle Game over state
-	 */
-	GameOver = (io, data, rooms, players) => {
-		return new Promise((resolve, reject) => {
-			const room = rooms.find((room) => room?.name === data?.room);
-			const player = players.find((player) => player?.username === data?.userName);
-            // console.log(players);
-            console.log("in send game over", room, player, player.gameOver);
-			if (room?.playersIn === 1) {
-                console.log("in 1")
-				room.state = false;
-				io.emit("update_rooms", rooms);
-			}
-            console.log("battle", room?.mode, player?.room, room?.name, !player.gameOver)
-			if (room?.mode === "battle" && player?.room === room?.name && !player.gameOver) {
-                console.log("in mode battle")
-				player.gameOver = true;
-				io.to(room.name).emit("chat", { message: `Player ${player?.username} loses`, type: "join" });
-				this.getroomUsersDetails(io, room.name, players).then((res) => {
-					const playersLose = players.filter((p) => p.room === room.name && p.gameOver);
-					if (res.length === playersLose.length) {
-						room.state = false;
-						playersLose.forEach((element) => {
-							element.gameOver = false;
-						});
-						io.emit("updateRooms", rooms);
-					}
-					if (res.length <= 5 && res.length >= 2 && res.length - 1 === playersLose.length) {
-						const playerWin = players.find((p) => p.room === room.name && !p.gameOver);
-						io.to(playerWin.socketId).emit("GameFinished", { winer: playerWin });
-						io.to(room.name).emit("chat", { message: `${playerWin?.username} WIN the game `, type: "join" });
-						room.state = false;
-						io.emit("updateRooms", rooms);
-						playersLose.forEach((element) => {
-							element.gameOver = false;
-						});
-					}
-				});
-			}
-		});
-	};
+     ** Handle Game over state
+     */
+    GameOver = (io, data, rooms, players) => {
+        // console.log(data, rooms, players);
+        return new Promise((resolve, reject) => {
+            const room = rooms.find((room) => room?.name === data?.room);
+            const player = players.find(
+                (player) => player?.username === data?.userName
+            );
+            console.log("player is", player);
+            console.log("room is", room);
+            // console.log("in send game over", room, player, player.gameOver);
+            if (room?.playersIn === 1) {
+                // console.log("in 1")
+                room.state = false;
+                console.log("1", rooms);
+                io.emit("update_rooms", { rooms: rooms });
+            }
+            // console.log("battle", room?.mode, player?.room, room?.name, !player.gameOver)
+            if (
+                room?.mode === "battle" &&
+                player?.room === room?.name &&
+                !player.gameOver
+            ) {
+                console.log("in mode battle");
+                player.gameOver = true;
+                io.to(room.name).emit("chat", {
+                    message: `Player ${player?.username} loses`,
+                    type: "join",
+                });
+                this.getroomUsersDetails(io, room.name, players).then((res) => {
+                    console.log("res", res);
+                    const playersLose = players.filter(
+                        (p) => p.room === room.name && p.gameOver
+                    );
+                    if (res.length === playersLose.length) {
+                        room.state = false;
+                        playersLose.forEach((element) => {
+                            element.gameOver = false;
+                        });
+                        console.log("2", rooms);
+                        io.emit("updateRooms", { rooms: rooms });
+                    }
+                    if (
+                        res.length <= 5 &&
+                        res.length >= 2 &&
+                        res.length - 1 === playersLose.length
+                    ) {
+                        const playerWin = players.find(
+                            (p) => p.room === room.name && !p.gameOver
+                        );
+                        io.to(playerWin.socketId).emit("GameFinished", {
+                            winer: playerWin,
+                        });
+                        io.to(room.name).emit("chat", {
+                            message: `${playerWin?.username} WIN the game `,
+                            type: "join",
+                        });
+                        room.state = false;
+                        console.log("3", rooms);
+                        io.emit("updateRooms", { rooms: rooms });
+                        playersLose.forEach((element) => {
+                            element.gameOver = false;
+                        });
+                    }
+                });
+            }
+        });
+    };
 }
 
 module.exports = GamesRoom;
--- server/index.js
+++ server/index.js
@@ -26,14 +26,11 @@ app.use(cors());
 
 io.on("connection", (socket) => {
     allClients.push(socket);
-    console.log("userConnected =>", socket.id);
     /*-------------ADD PLAYER-------------*/
     socket.on("addPlayerRequest", (data) => {
-        // //console.log(data);
         const exist = players.find(
             (player) => player.username === data.username
         );
-        // //console.log(exist);
         if (!exist) {
             players = [
                 ...players,
@@ -59,14 +56,12 @@ io.on("connection", (socket) => {
     });
     /*-------------ADD ROOM-------------*/
     socket.on("createRoomRequest", async (data) => {
-        // console.log(data);
         const exist = rooms.find((room) => room.name === data.room);
         const player = players.find(
             (player) =>
                 player.username === data.username &&
                 player.socketId === socket.id
         );
-        // console.log("player before create room", player);
         if (!exist) {
             if (data.mode === "battle")
                 rooms = [
@@ -93,7 +88,6 @@ io.on("connection", (socket) => {
                     },
                 ];
             if (player) {
-                // console.log("jlayer");
                 player.room = data?.room;
                 player.admin = true;
             }
@@ -106,7 +100,7 @@ io.on("connection", (socket) => {
             });
             io.emit("updateRooms", { rooms: rooms });
         } else {
-            socket.emit("room_exists");
+            socket.emit("roomExists");
         }
     });
     /*-------------JOIN ROOM-------------*/
@@ -130,7 +124,64 @@ io.on("connection", (socket) => {
             }
         }
     });
-
+    /*-------------JOIN ROOM-------------*/
+    socket.on("joinRoomFromLink", ({ username, room }) => {
+        const exist = players.find((player) => player.username === username);
+        if (!exist) {
+            players = [
+                ...players,
+                {
+                    username: username,
+                    socketId: socket.id,
+                    avatar: "Rhett_James.png",
+                    room: "",
+                    admin: false,
+                },
+            ];
+            socket.emit("addPlayerSuccess", {
+                username: username,
+                avatar: "Rhett_James.png",
+            });
+            const joinedRoom = rooms.find((r) => r.name === room);
+            if (joinedRoom) {
+                if (joinedRoom.mode === "battle" && joinedRoom.playersIn < 5) {
+                    socket.join(room);
+                    players[players.length - 1].room = room;
+                    players[players.length - 1].admin = false;
+                    joinedRoom.playersIn += 1;
+                    socket.emit("roomJoinedSuccess", room);
+                    io.to(room).emit("chat", {
+                        message: `Player ${username} joined the room ${room}`,
+                        type: "join",
+                    });
+                    io.emit("updateRooms", { rooms: rooms });
+                }
+            } else {
+                rooms = [
+                    ...rooms,
+                    {
+                        name: room,
+                        mode: "battle",
+                        maxPlayers: 5,
+                        playersIn: 1,
+                        state: false,
+                        stages: [],
+                    },
+                ];
+                players[players.length - 1].room = room;
+                players[players.length - 1].admin = true;
+                socket.join(room);
+                socket.emit("createRoomSucces", room);
+                io.to(room).emit("chat", {
+                    message: `Player ${username} created the room ${room}`,
+                    type: "join",
+                });
+                io.emit("updateRooms", { rooms: rooms });
+            }
+        } else {
+            socket.emit("addPlayerFail", { error: "user existe" });
+        }
+    });
     /*-------------GET PLAYERS-------------*/
     socket.on("getPlayers", (data) => {
         var temp = [];
@@ -153,8 +204,17 @@ io.on("connection", (socket) => {
     /*-------------START THE GAME-------------*/
     socket.on("startgame", (data) => {
         const room = rooms.find((room) => room.name === data);
+        game.getroomUsersDetails(io, room.name, players).then((res) => {
+            console.log(res);
+            for (let i = 0; i < players.length; i++) {
+                for (let j = 0; j < res.length; j++) {
+                    if (players[i].username === res[j].username)
+                        players[i].gameOver = false;
+                }
+            }
+        });
         game.getUser(io, socket.id, room, players).then(async (user) => {
-            // console.log("the user is", user);
+            // //console.log("the user is", user);
             if (user.admin) {
                 const tetros = await tetrominos.getTetriminos();
                 game.startGame(io, room, tetros);
@@ -176,37 +236,225 @@ io.on("connection", (socket) => {
             type: "message",
         });
     });
-    //new tetros
+    /*-------------NEW TETROS-------------*/
     socket.on("newTetriminos", async (data) => {
         const tetriminos = await tetrominos.getTetriminos();
-        // console.log(data, tetriminos)
+        // //console.log(data, tetriminos)
         game.newTetriminos(io, data, tetriminos);
     });
 
-    //add stage
+    /*-------------ADD STAGE-------------*/
     socket.on("sendStage", async (data) => {
-        // console.log(data);
+        // //console.log(data);
         const player = players.find((p) => p.username === data.username);
-        // console.log(player);
+        // //console.log(player);
         if (player && player.room === data.room) {
             game.sendStage(io, data.room, data.stage, data.username);
         }
     });
 
+    /*-------------ADD WALL-------------*/
     socket.on("addWall", async (data) => {
-        // console.log("some data to wall", data);
+        // //console.log("some data to wall", data);
         game.addWall(socket, data.room);
     });
-
+    /*-------------GAME OVER-------------*/
     socket.on("GameOver", async (data) => {
-        console.log("gameOver", data);
+        //console.log("gameOver", data);
         game.GameOver(io, data, rooms, players);
-      });
+    });
+    /*-------------LEAVE ROOM-------------*/
+    socket.on("leaveRoom", (data) => {
+        const room = rooms.find((room) => room.name === data.roomName);
+        let indexOfRoom = rooms.indexOf(room);
+        if (room) {
+            if (
+                room.mode === "solo" ||
+                (room.mode === "battle" && room.playersIn <= 1)
+            )
+                rooms.splice(indexOfRoom, 1);
+            else {
+                var temp = [];
+                var roomPlayers = [];
+                const clients = io.sockets.adapter.rooms.get(data.roomName);
+                if (clients) {
+                    for (const clientId of clients) {
+                        temp.push(clientId);
+                    }
+                }
+                for (let i = 0; i < temp.length; i++) {
+                    for (let j = 0; j < players.length; j++) {
+                        if (players[j].socketId === temp[i]) {
+                            roomPlayers.push(players[j]);
+                        }
+                    }
+                }
+
+                socket.leave(data.roomName);
+                let playerLeft = roomPlayers.find(
+                    (s) => s.username === data.userName
+                );
+                let playerLeftIndex = roomPlayers.indexOf(playerLeft);
+                roomPlayers.splice(playerLeftIndex, 1);
+                let jet = players.indexOf(
+                    players.find((p) => p?.username === playerLeft.username)
+                );
+                players[jet].room = "";
+                console.log("jet", players[jet]);
+                if (data.admin) {
+                    let pIndex = players.indexOf(
+                        players.find(
+                            (p) => p?.username === roomPlayers[0].username
+                        )
+                    );
+                    players[pIndex].admin = true;
+                    pIndex = players.indexOf(
+                        players.find((p) => p?.username === playerLeft.username)
+                    );
+                    players[pIndex].admin = false;
+                    players[pIndex].room = "";
+                }
+                if (roomPlayers.length < 1) {
+                    rooms.splice(indexOfRoom, 1);
+                } else {
+                    rooms[indexOfRoom].playersIn -= 1;
+                    io.to(data.roomName).emit("playerLeftRoom", {
+                        roomPlayers,
+                        playerLeft: { ...playerLeft, admin: data.admin },
+                    });
+                    if (
+                        roomPlayers.length == 1 &&
+                        rooms[indexOfRoom].state == true
+                    ) {
+                        rooms[indexOfRoom].state = false;
+                        io.to(roomPlayers[0].socketId).emit("GameFinished", {
+                            winer: roomPlayers[0],
+                        });
+                    } else if (
+                        rooms[indexOfRoom].playersIn == 2 &&
+                        rooms[indexOfRoom].state == true
+                    ) {
+                        let s = roomPlayers.find((p) => p?.gameOver == true);
+                        if (s) {
+                            let i = roomPlayers.indexOf(s);
+                            let v = i == 0 ? 1 : 0;
+                            rooms[indexOfRoom].state = false;
+                            io.to(roomPlayers[v].socketId).emit(
+                                "GameFinished",
+                                {
+                                    winer: roomPlayers[v],
+                                }
+                            );
+                        }
+                    }
+                }
+            }
+            io.to(data.roomName).emit("updatePlayers", roomPlayers);
+            socket.emit("leaveRoomSuccess");
+            io.emit("updateRooms", { rooms });
+        }
+    });
     /*-------------DISCONNECT SOCKET-------------*/
     socket.on("disconnect", () => {
-        var i = allClients.indexOf(socket);
-        allClients.splice(i, 1);
-        console.log("User disconnected =>", socket.id);
+        let varPlayer = players?.find((player) => player.socketId == socket.id);
+        if (varPlayer?.username && varPlayer.room.length > 0) {
+            let isAdmin = varPlayer?.admin;
+            const varRoom = rooms.find((room) => room.name === varPlayer.room);
+            let indexOfRoom = rooms.indexOf(varRoom);
+            if (varRoom) {
+                if (
+                    varRoom.mode === "solo" ||
+                    (varRoom.mode === "battle" && varRoom.playersIn <= 1)
+                )
+                    rooms.splice(indexOfRoom, 1);
+                else {
+                    var temp = [];
+                    var roomPlayers = [];
+                    const clients = io.sockets.adapter.rooms.get(varRoom.name);
+                    if (clients) {
+                        for (const clientId of clients) {
+                            temp.push(clientId);
+                        }
+                    }
+                    console.log(clients);
+                    for (let i = 0; i < temp.length; i++) {
+                        for (let j = 0; j < players.length; j++) {
+                            if (players[j].socketId === temp[i]) {
+                                roomPlayers.push(players[j]);
+                            }
+                        }
+                    }
+                    socket.leave(varRoom.name);
+                    let jet = players.indexOf(
+                        players.find((p) => p?.username === varPlayer.username)
+                    );
+                    players[jet].room = "";
+                    if (varPlayer.admin) {
+                        console.log(roomPlayers);
+                        let pIndex = players.indexOf(
+                            players.find(
+                                (p) => p?.username === roomPlayers[0].username
+                            )
+                        );
+                        players[pIndex].admin = true;
+                        pIndex = players.indexOf(
+                            players.find(
+                                (p) => p?.username === varPlayer.username
+                            )
+                        );
+                        players[pIndex].admin = false;
+                        players[pIndex].room = "";
+                    }
+                    if (roomPlayers.length < 1) {
+                        rooms.splice(indexOfRoom, 1);
+                    } else {
+                        console.log(varRoom.name);
+                        rooms[indexOfRoom].playersIn -= 1;
+                        io.to(varRoom.name).emit("playerLeftRoom", {
+                            roomPlayers,
+                            playerLeft: {
+                                ...varPlayer,
+                                admin: isAdmin,
+                            },
+                        });
+                        if (
+                            roomPlayers.length == 1 &&
+                            rooms[indexOfRoom].state == true
+                        ) {
+                            rooms[indexOfRoom].state = false;
+                            io.to(roomPlayers[0].socketId).emit(
+                                "GameFinished",
+                                {
+                                    winer: roomPlayers[0],
+                                }
+                            );
+                        } else if (
+                            rooms[indexOfRoom].playersIn == 2 &&
+                            rooms[indexOfRoom].state == true
+                        ) {
+                            let s = roomPlayers.find(
+                                (p) => p?.gameOver == true
+                            );
+                            if (s) {
+                                let i = roomPlayers.indexOf(s);
+                                let v = i == 0 ? 1 : 0;
+                                rooms[indexOfRoom].state = false;
+                                io.to(roomPlayers[v].socketId).emit(
+                                    "GameFinished",
+                                    {
+                                        winer: roomPlayers[v],
+                                    }
+                                );
+                            }
+                        }
+                    }
+                }
+                io.to(varRoom.name).emit("updatePlayers", roomPlayers);
+                socket.emit("leaveRoomSuccess");
+                io.emit("updateRooms", { rooms });
+            }
+        }
+        players = players.filter((i) => i !== varPlayer);
     });
 });
 
